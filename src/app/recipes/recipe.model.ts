9dbae3ba03bb2781881c12b0325a2bfdbac75a60
mecozma <mecozma@gmail.com>
2021-02-01 21:03:50 +0000

    Added a recipe model and modified the navbar to untill I'll add a hamburger menu.

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+import { Settings } from "http2";
+
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, imagePath: string) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+    }
+}
